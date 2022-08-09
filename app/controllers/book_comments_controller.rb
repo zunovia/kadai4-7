d1f6f35d48a132299dc4d291471c26a18100e2bf
zunovia <105263325+zunovia@users.noreply.github.com>
2022-08-09 04:59:02 +0000

    2022.8.9 last

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,19 +1,17 @@
 class BookCommentsController < ApplicationController
+ before_action :authenticate_user!
+
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    comment.save
-    #redirect_to request.referer
+    @book = Book.find(params[:book_id])
+    @comment = current_user.book_comments.new(book_comment_params)
+    @comment.book_id = @book.id
+    @comment.save
   end
 
-   def destroy
-        refroute = Rails.application.routes.recognize_path(request.referrer)
-        @book = Book.find(refroute[:id])
-        BookComment.find(params[:book_id]).destroy
-        render :destroy
-   end
-
+  def destroy
+    @comment = BookComment.find(params[:id])
+    @comment.destroy
+  end
 
   private
   def book_comment_params
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,17 +1,17 @@
 class FavoritesController < ApplicationController
-  before_action :authenticate_user!
+   before_action :authenticate_user!
 
   def create
     @book = Book.find(params[:book_id])
-    favorite = current_user.favorites.new(book_id: @book.id)
+    favorite = @book.favorites.new(user_id: current_user.id)
     favorite.save
-    #redirect_to request.referer
+    # app/views/favorites/create.js.erbを参照する
   end
 
   def destroy
     @book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id: @book.id)
+    favorite = @book.favorites.find_by(user_id: current_user.id)
     favorite.destroy
-    #redirect_to request.referer
+    # app/views/favorites/destroy.js.erbを参照する
   end
 end
